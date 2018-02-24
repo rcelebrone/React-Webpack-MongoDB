d1763f580e5bcc8ccc0185ff1c837b3cbde69f7f
rcelebrone <rcelebrone@gmail.com>
2018-02-24 15:42:58 -0300

    melhorias de UX

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -7,12 +7,12 @@ import TodoList from '../todo/todoList';
 const __urlApi = 'http://localhost:3005/api/todos';
 
 export default class Todo extends Component {
-    
-    constructor(props){
+
+    constructor(props) {
         super(props);
 
         //crio o estado inicial (as propriedades) dessa classe
-        this.state = {description: '', list:[]};
+        this.state = {description: '', list:[], filtered: false};
 
         //independente de quem chama o handleAdd, o this é a class Todo agora
         this.handleAdd = this.handleAdd.bind(this);
@@ -21,6 +21,7 @@ export default class Todo extends Component {
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
         this.handleSearch = this.handleSearch.bind(this);
+        this.handleClear = this.handleClear.bind(this);
 
         this.refresh();
     }
@@ -29,38 +30,55 @@ export default class Todo extends Component {
         const search = (description ? `&description__regex=/${description}/` : '');
 
         Axios.get(`${__urlApi}?sort=-createdAt${search}`)
-            .then(r => this.setState({...this.state, description: this.state.description, list: r.data}));
+            .then(r => this.setState({...this.state, description: this.state.description, list: r.data, filtered: false}));
     }
 
     handleAdd() {
         const description = this.state.description;
         Axios.post(__urlApi, { description })
-            .then(r => this.refresh());
+            .then(r => this.handleClear());
     }
 
     handleChange(e) {
-        this.setState({...this.state, description: e.target.value});
+        this.setState({...this.state, description: e.target.value, filtered: false});
     }
 
     handleSearch() {
         this.refresh(this.state.description);
+        this.setState({...this.state, filtered: true});
     }
 
     handleRemove(todo) {
         Axios.delete(`${__urlApi}/${todo._id}`)
-            .then(r => this.refresh(this.state.description));
+            .then(r => this.refreshIfNotFiltered());
     }
 
     handleMarkAsDone(todo) {
         Axios.put(`${__urlApi}/${todo._id}`, { ...todo, done: true })
-            .then(r => this.refresh(this.state.description));
+            .then(r => this.refreshIfNotFiltered());
     }
 
     handleMarkAsPending(todo) {
         Axios.put(`${__urlApi}/${todo._id}`, { ...todo, done: false })
-            .then(r => this.refresh(this.state.description));
+            .then(r => this.refreshIfNotFiltered());
     }
-    
+
+    refreshIfNotFiltered() {
+        const description = this.state.description;
+
+        if(this.state.filtered)
+            this.refresh(description);
+        else
+        {
+            this.refresh();
+            this.setState({...this.state, description: description, filtered: false});
+        }
+    }
+
+    handleClear(){
+        this.refresh();
+        this.setState({...this.state, description: '', filtered: false});
+    }
 
     render() {
         return (
@@ -70,6 +88,7 @@ export default class Todo extends Component {
                     handleAdd={this.handleAdd} 
                     handleChange={this.handleChange}
                     handleSearch={this.handleSearch}
+                    handleClear={this.handleClear}
                     description={this.state.description} />
                 <hr />
                 <TodoList
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -2,22 +2,34 @@ import React from 'react';
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
 
-export default (props) => (
-    <div role="form" className="todoForm row">
-        <Grid cols="12,9,10">
-            <input id='description' 
-                className='form-control' 
-                placeholder='Adicione ou pesquise uma tarefa'
-                value={props.description}
-                onChange={props.handleChange} />
-            <br />
-            <sup>
-                [Shit + Enter]=> pesquisa uma tarefa | [Enter]=> adiciona tarefa
-            </sup>
-        </Grid>
-        <Grid cols="12,3,2">
-            <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
-            <IconButton style="success" icon="search" onClick={props.handleSearch} />
-        </Grid>
-    </div>
-)
+export default (props) => {
+
+    const actions = (e) => {
+        if(e.key === 'Enter')
+            e.shiftKey ? props.handleSearch() : props.handleAdd();
+        else if(e.key === 'Escape')
+            props.handleClear();
+    }
+
+    return (
+        <div role="form" className="todoForm row">
+            <Grid cols="12,9,10">
+                <input id='description' 
+                    className='form-control' 
+                    placeholder='Adicione ou pesquise uma tarefa'
+                    value={props.description}
+                    onKeyUp={actions}
+                    onChange={props.handleChange} />
+                <br />
+                <sup>
+                    [Shit + Enter]=> pesquisa uma tarefa | [Enter]=> adiciona tarefa | [Esc]=> Limpa a busca
+                </sup>
+            </Grid>
+            <Grid cols="12,3,2">
+                <IconButton style="primary" icon="plus" onClick={props.handleAdd} />
+                <IconButton style="success" icon="search" onClick={props.handleSearch} />
+                <IconButton style="default" icon="close" onClick={props.handleClear} />
+            </Grid>
+        </div>
+    );
+}
